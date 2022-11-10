9071c155078976d555a3bcb247edfcc8586fb0c3
Nikostreet11 <nico.pollini@gmail.com>
2022-11-10 16:55:09 +0100

    Add dummy meanShift function with simple array allocation and print

--- main.cpp
+++ main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+// receive matrix of points in any multidimensional space
+// TODO return vector of cluster indices
+int* meanShift(float** points, size_t nOfPoints, size_t dimension)
+{
+	// just print the array
+	cout << __func__ << endl;
+	for (size_t i = 0; i < nOfPoints; ++i)
+	{
+		cout << i << ":" << '\t';
+		for (size_t j = 0; j < dimension; ++j)
+			cout << points[i][j] << '\t';
+		cout << endl;
+	}
+	return NULL;
+}
+
+int main()
+{
+	size_t nOfPoints = 100;
+	size_t dimension = 5;
+
+	// data initialization
+	// TODO a bit clunky, use struct/2D vector instead?
+	srand((unsigned) time(NULL));
+	float** points = new float*[nOfPoints];
+	for (int i = 0; i < nOfPoints; ++i)
+	{
+		points[i] = new float[dimension];
+		for (int j = 0; j < dimension; ++j)
+			points[i][j] = (float) rand()/RAND_MAX * 1000;
+	}
+
+	meanShift(points, nOfPoints, dimension);
+
+	// deallocate memory
+	for (int i = 0; i < nOfPoints; ++i) {
+		delete[] points[i];
+	}
+	delete[] points;
+
+	return 0;
 }
